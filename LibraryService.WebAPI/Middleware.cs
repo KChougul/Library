a4bfa73d301b72e5560d8230e3fc0c2995a5b308
KChougul <69729805+KChougul@users.noreply.github.com>
2020-08-16 00:09:11 +0530

    Update Middleware.cs

--- LibraryService.WebAPI/Middleware.cs
+++ LibraryService.WebAPI/Middleware.cs
@@ -5,15 +5,24 @@ namespace LibraryService.WebAPI
 {
     public class Middleware
     {
+        public int counter = 0;
+        private readonly RequestDelegate nextRequest;
 
         public Middleware(RequestDelegate next)
         {
-           
+           nextRequest = next;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
-          
+            
+          counter = counter + 1;
+            context.Response.OnStarting(state=>{
+                var httpContext = (HttpContext)state;
+            httpContext.Response.Header.Add("requestCounter", new[]{counter});
+                return Task.CompletedTask;
+            },context);
+            await nextRequest(context);
         }
     }
 }
